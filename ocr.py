0104ae5d39dbfe5449158eb374d18f1b05b967e2
jerghy <113653561+jerghy@users.noreply.github.com>
2025-03-15 12:58:23 +0800

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -1,7 +1,10 @@
 import requests
 from requests.auth import HTTPBasicAuth
 from tqdm import tqdm
-
+import fitz  # PyMuPDF
+import os
+import json
+from imgocr import ImgOcr
 
 name="高中必刷题数学人教A版必修1.pdf"
 # url = "https://chogo.teracloud.jp/dav/documents/output.mp3"
@@ -20,8 +23,7 @@ else:
     print(f"下载失败，状态码：{response.status_code}")
 
 
-import fitz  # PyMuPDF
-import os
+
 
 def pdf_to_images(pdf_path, output_folder):
     # 创建输出文件夹
@@ -47,29 +49,25 @@ output_folder = f'output_{pdf_name.split(".")[0]}'
 pdf_to_images(pdf_path, output_folder)
 
 
-
-from imgocr import ImgOcr
-# import cv2
-import json
-# 初始化OCR模型
 ocr = ImgOcr()
+outputrelease={}
+pdf_document = fitz.open(pdf_path)
+# 遍历每一页range(len(pdf_document))
+for page_num in range(3):
+    pg=f'{output_folder}/page_{page_num+1}.png'
+    result = ocr.ocr(pg)
+    print(pg)
+    outputrelease[pg]=result
+
+
+
+
 
 # 读取图片并进行OCR识别
-image_path = "input2.png"
-result = ocr.ocr(image_path)
 
-# 打印识别结果
-for line in result:
-    # box, text, score = line
-    box=line["box"]
-    text=line["text"]
-    score=line["score"]
-    # print(f"识别文本: {text}, 置信度: {score:.4f}")
-    # print(f"识别文本: {text}, 置信度: ")
-    print(f"位置：{box}，文字：“{text}”")
 # 保存result到JSON文件
 with open("result.json", "w", encoding="utf-8") as f:
-    json.dump(result, f, ensure_ascii=False, indent=4)
+    json.dump(outputrelease, f, ensure_ascii=False, indent=4)
 
 
 # 可选：在图片上绘制识别框并保存
